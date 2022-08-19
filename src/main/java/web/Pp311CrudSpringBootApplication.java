f5e05fa6de15418fa25d282f344303969d2e2010
Khaizar1 <khaizar@mail.ru>
2022-08-19 21:41:47 +0300

    commit #1

---
+++ src/main/java/web/Pp311CrudSpringBootApplication.java
@@ -0,0 +1,11 @@
+package web;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp311CrudSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Pp311CrudSpringBootApplication.class, args);
+    }
+
+}
